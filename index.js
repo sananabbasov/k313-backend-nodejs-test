e9d75148bde919005de02f7e4d59ce7f469721ed
sananabbasov <sanan_abbasov@list.ru>
2022-08-24 19:15:14 +0400

    24.08.2022

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const movies = require('./data/movies.json')
+const series = require('./data/series.json')
+const e = require('express')
+const app = express()
+app.use(cors())
+
+
+app.get('/movies', function (req, res) {
+    res.send(movies)
+})
+app.get('/series', function (req, res) {
+    if (req.query.id == undefined) {
+        res.send(series)
+    }else{
+        res.send(series.find(x=>x.id == req.query.id))
+    }
+})
+
+
+
+app.listen(5000)
